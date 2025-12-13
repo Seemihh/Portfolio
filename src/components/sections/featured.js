1bc1284f0bd6a58c4518744a2b8e3a306d69dcd6
Seemihh <semih.ozkara60@gmail.com>
2025-12-13 17:51:55 +0300

    All content updated to Semih Ozkara's profile (Projec554)

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -314,7 +314,11 @@ const Featured = () => {
           node {
             frontmatter {
               title
-              cover
+              cover {
+                childImageSharp {
+                  gatsbyImageData(width: 700, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                }
+              }
               tech
               github
               external
